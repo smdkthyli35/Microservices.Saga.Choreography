e8d749d1bb22c7b12d0cb045bb6a87c83c322ac5
Samed Kütahyalı <smdkthyli@gmail.com>
2024-03-30 16:29:30 +0300

    add payment api processes

--- Shared/RabbitMQSettings.cs
+++ Shared/RabbitMQSettings.cs
@@ -10,5 +10,6 @@ namespace Shared
     {
         public const string Stock_OrderCreatedEventQueue = "stock-order-created-event-queue";
         public const string Payment_StockReservedEventQueue = "payment-stock-reserved-event-queue";
+        public const string Order_PaymentCompletedEventQueue = "order-payment-completed-event-queue";
     }
 }
